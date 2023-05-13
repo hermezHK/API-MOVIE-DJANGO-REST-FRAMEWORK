7afb60f562163b53dba4944f8a05ce9743a11483
Hermez <angelo_stk06@hotmail.com>
2023-05-13 12:08:49 -0500

    feat: create model movies/ add license and gitignore

---
+++ movie/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    year = models.IntegerField()
+    rating = models.FloatField()
+    genre = models.CharField(max_length=50)
+    image = models.TextField()
+    country = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField()
+
+    class Meta:
+        db_table = "movies"
